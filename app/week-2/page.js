f13b5eb2124e8cd4f6648e9ed5f25e89c8422327
mehakdeep-01 <mehakdeepkaur02@edu.sait.ca>
2025-01-18 13:23:30 -0600

    commit code

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from './student-info';
+
+export default function Page() {
+    return (
+      <main>
+        <StudentInfo />
+        <h1>Shopping List</h1>
+        
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import React from "react";
+
+export default function StudentInfo()
+{
+    return(
+        
+        <div>
+            <h2>
+                Mehakdeep Kaur
+            </h2> 
+            <p>
+                <Link href="\">My Github repository</Link>
+            </p>
+        </div>
+    );
+}
+
+
