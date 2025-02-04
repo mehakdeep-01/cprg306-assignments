8e880f3fdddfd8a314ddac13a34a6c2a4d494c2b
mehakdeep-01 <mehakdeepkaur02@edu.sait.ca>
2025-02-04 13:06:29 -0600

    commit week-3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,10 +3,8 @@ export default function Item(props) {
     return (
         <li className="bg-pink-200 p-4 m-4">
             <h2 className="font-bold text-blue-900"> {props.name}</h2>
-            <p className="text-sm text-gray-600">
-                <span className="font-bold">Quantity:</span>{props.quantity}</p>
-            <p className="text-sm text-gray-600">
-                <span className="font-bold">Category:</span> {props.category}</p>
+            <p className="text-gray-600">
+                <span className="font-bold"> Buy {props.quantity} in {props.category}</span></p>
         </li>
     );
 }
